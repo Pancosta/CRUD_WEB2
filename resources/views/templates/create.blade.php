bdfef167c10823ae92184d11c2e74c78a4875148
Pancosta <dz6.gustav@gmail.com>
2023-05-10 00:18:07 -0300

    inimigo do front kkk

--- resources/views/templates/create.blade.php
+++ resources/views/templates/create.blade.php
@@ -1,19 +1,20 @@
+<head>  
+    <style>  
+        .bg-dark {
+            background-color: #1F2937;
+            color: #FFFFFF;
+        }
+    </style>    
+<head>
 @extends('layouts.app')
 
-@section('content')
-    <div class="row">
-        <div class="col-lg-12 margin-tb">
-            <div class="pull-left">
-                <h2>Criar Novo Template</h2>
-            </div>
-            <div class="pull-right">
-                <a class="btn btn-primary" href="{{ route('templates.index') }}"> Voltar</a>
-            </div>
-        </div>
+@section('content')                                   
+    <div class="flex justify-between items-center mb-8">
+        <h2 class="text-white-2xl font-bold">Criar Novo Template</h2>
+        <a class="inline-block py-2 px-4 text-white bg-blue-500 hover:bg-blue-600 rounded" href="{{ route('templates.index') }}">Voltar</a>
     </div>
-
     @if ($errors->any())
-        <div class="alert alert-danger">
+        <div class="bg-red-100 text-red-900 rounded-md p-4 mb-8">
             <strong>Ops!</strong> Ocorreu um erro com os dados informados.<br><br>
             <ul>
                 @foreach ($errors->all() as $error)
@@ -23,26 +24,25 @@
         </div>
     @endif
 
-    <form action="{{ route('templates.store') }}" method="POST">
-        @csrf
+    <div class="max-w-lg mx-auto mb-8">
+        <form action="{{ route('templates.store') }}" method="POST">
+            @csrf
 
-        <div class="row">
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Nome:</strong>
-                    <input type="text" name="nome" class="form-control" placeholder="Informe o nome">
-                </div>
+            <div class="mb-4">
+                <label for="nome" class="block text-white-700 font-bold mb-2">Nome:</label>
+                <input type="text" name="nome" id="nome" class="w-full border-gray-400 border-2 rounded px-3 py-2" placeholder="Informe o nome">
             </div>
-            <div class="col-xs-12 col-sm-12 col-md-12">
-                <div class="form-group">
-                    <strong>Modelo:</strong>
-                    <input type="text" name="modelo" class="form-control" placeholder="Informe o modelo">
-                </div>
-            </div>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Salvar</button>
-            </div>
-        </div>
 
-    </form>
+            <div class="mb-8">
+                <label for="modelo" class="block text-white-700 font-bold mb-2">Modelo:</label>
+                <input type="text" name="modelo" id="modelo" class="w-full border-gray-400 border-2 rounded px-3 py-2" placeholder="Informe o modelo">
+            </div>
+
+            <div class="flex justify-center">
+                <button type="submit" class="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded">Salvar</button>
+            </div>
+
+        </form>
+    </div>
+
 @endsection
--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -1,40 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h2>Lista de Templates</h2>
-        <a href="{{ route('templates.create') }}" class="btn btn-success mb-2">Novo Template</a>
+<div class="container">
+    <h2 class="text-2xl font-bold mb-4">Lista de Templates</h2>
+    <a href="{{ route('templates.create') }}" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mb-4">Novo Template</a>
 
-        <table class="table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nome</th>
-                    <th>Modelo</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach($templates as $template)
-                <tr>
-                    <td>{{ $template->id }}</td>
-                    <td>{{ $template->nome }}</td>
-                    <td>{{ $template->modelo }}</td>
-                    <td>
-                        <form action="{{ route('templates.destroy', $template->id) }}" method="POST">
-                            <a href="{{ route('templates.edit', $template->id) }}" class="btn btn-primary">Editar</a>
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja excluir este template?')">Excluir</button>
-                        </form>
-                    </td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-
-        <div class="d-flex justify-content-center">
-            {{ $templates->links() }}
-        </div>
-    </div>
+    @if ($templates)
+    <table class="table-auto w-full">
+        <thead>
+            <tr>
+                <th class="px-4 py-2">#</th>
+                <th class="px-4 py-2">Nome</th>
+                <th class="px-4 py-2">Modelo</th>
+                <th class="px-4 py-2">Ações</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($templates as $template)
+            <tr>
+                <td class="border px-4 py-2">{{ $template->id }}</td>
+                <td class="border px-4 py-2">{{ $template->nome }}</td>
+                <td class="border px-4 py-2">{{ $template->modelo }}</td>
+                <td class="border px-4 py-2">
+                    <form action="{{ route('templates.destroy', $template->id) }}" method="POST">
+                        <a href="{{ route('templates.edit', $template->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2">Editar</a>
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onclick="return confirm('Tem certeza que deseja excluir este template?')">Excluir</button>
+                    </form>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    {{ $templates->links() }}
+    @else
+    <p>Nenhum template encontrado.</p>
+    @endif
+</div>
 @endsection
